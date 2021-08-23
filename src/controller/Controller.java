ead7016b6fd525c22a3a9e3c875820a662be5788
rafae <rafae@DESKTOP-GBGPNIQ>
2021-08-23 02:19:44 -0300

    ED_Ex01_SomaNaturais

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,10 +7,12 @@ public class Controller {
 		}
 		
 		public int resultado(int fatSoma) {
-			//Condicao de parada =  0 se a entrada for igual ou menor que 0, devera retornar 0
+/*		 Condição de parada =  0 se a entrada for igual
+		 ou menor que 0, deverá retornar 0 */
 		 if (fatSoma <= 0) {
 	      return 0;	
-	      //Caso seja maior que 0, deverá somar fatSoma + (fatSoma - 1)..
+/*		 Caso seja maior que 0, deverá somar
+         valor da entrada + (valor da entrada - 1)*/
 		 }else {
 			 
 			 return fatSoma + resultado(fatSoma - 1);
