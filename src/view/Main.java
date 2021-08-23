84995c2eb626a2807e57e3100ae7b05318073f1b
rafae <rafae@DESKTOP-GBGPNIQ>
2021-08-23 02:27:12 -0300

    ED_Ex01_SomaNaturais

--- src/view/Main.java
+++ src/view/Main.java
@@ -7,11 +7,16 @@ import controller.Controller;
 public class Main {
 
 	public static void main(String[] args) {
-		//Metodo construtor
+//		Metodo construtor
 		Controller fatSoma = new Controller();
-		//Definindo um valor para a soma
-		int somaNat = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o numero desejado: "));
-		//variavel que recebe o resultado
+//		Definindo um valor para a soma
+		int somaNat = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número natural: "));
+//		Condição para entrada apenas de valores inteiros positivos
+		if(somaNat < 0) {
+			System.err.println("Apenas valores inteiros e positivos!");
+			System.exit(0);
+		}
+//		variavel que recebe o resultado
 		int somaNaturais = fatSoma.resultado(somaNat);
 		System.out.println("Resultado: "+somaNaturais);
 	}
